f95d584d90a2b722691866b5e31915009673e0b4
Pedro Ivo <cinelli.dev@gmail.com>
2022-04-21 09:37:06 -0300

    feat post sign up & post tweets added

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
+let usersData = [];
+let usersTweets = [];
 
+app.post("/sign-up", (req, res) => {
+  usersData.push(req.body);
+  res.send("Ok");
+});
 
-app.listen(5000)
+app.post("/tweets", (req, res) => {
+  console.log(req.body);
+  // const findUser = usersData.find((user) => {
+  //   req.body.username === user.username;
+  // });
+  let findUser = ""
+
+  usersData.map((user) => {
+    if(user.username === req.body.username) {
+      findUser = user.avatar
+    }
+  })
+
+  let newTweet = {...req.body, avatar: findUser}
+
+  usersTweets.push(newTweet);
+  res.send("Ok");
+});
+
+app.listen(5000);
